0b0c88a971a4bec254d98ba7404d9f8892dfd660
Jonathan Miranda <jmiran15@jhu.edu>
2024-02-20 16:01:24 -0500

    test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <iframe
+        id="chatmate-chatbot-widget-iframe"
+        style="
+position: fixed;
+bottom: 8px;
+right: 8px;
+width: 80px;
+height: 80px;
+border: none;
+z-index: 1000;"
+        src="https://chatmate.fly.dev/e95f87ab-d525-4a8e-b9ca-4fe17e5c3d23/widget"
+        title="chatbot-preview"
+      ></iframe>
+      <script src="https://chatmate.fly.dev/iframeResizer.js"></script>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
